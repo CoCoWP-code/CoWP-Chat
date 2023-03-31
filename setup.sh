953c9dc3f01854cf0c2f1eb752b53e27a947641d
CoCoWP-code <vipdaklak2015@gmail.com>
2023-03-31 16:16:34 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,41 +10,23 @@ print_status() {
     echo "## $1"
     echo
 }
-sudo apt update 
-print_status "Update System..."
-sudo apt install tmux
-print_status "Installing the install tmux..."
 
-sudo curl -sSL https://deb.nodesource.com/setup_18.x | sudo bash -
+apt  install -y docker-compose
+sudo curl -sSL https://deb.nodesource.com/setup_18.x | sudo bash - 
 sudo apt install -y nodejs 
-
-print_status "Installing the NodeSource NODEJS repo..."
-
-sudo npm install pnpm -g
-print_status "Installing the pnpm..."
-
+sudo apt install -y tmux
+sudo npm install -y pnpm -g 
+mkdir /var/www
+cd /var/www
 sudo git clone https://github.com/CoCoWP-code/CoWP-Chat.git 
-cp 
-cd CoWP-Chat/ 
+mv /var/www/CoWP-Chat /var/www/html
+cd html/ 
 sudo pnpm bootstrap 
 cd service/ 
-cp .env.example .env
-
-# Ask user for yes/no response
-read -p "Bạn cần nhập API và token của OpenAI vào file Service/.evn sau đó nhấn Yes " answer
-
-# Check user response using if-else statement
-if [[ "$answer" =~ ^(yes|y)$ ]]
-then
-    echo "Continuing..."
 sudo pnpm install 
 cd  
-cd CoWP-Chat/ 
-tmux new-session -s mysession 
+cd /var/www/html/docker-compose
+docker-compose up -d 
+cd /var/www/html 
+sudo tmux new-session -s mysession 
 sudo bash start.sh 
-elif [[ "$answer" =~ ^(no|n)$ ]]
-then
-    echo "Stopping..."
-else
-    echo "Invalid input, stopping..."
-fi
